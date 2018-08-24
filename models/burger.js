07959d4362cfaf05b01e41625c00f2415eaa7f59
Rich Schiffer <richschiffer@Richs-MacBook-Pro.local>
2018-08-23 18:04:17 -0600

    fixed node_modules

--- models/burger.js
+++ models/burger.js
@@ -3,19 +3,19 @@ const orm = require("../config/orm.js");
 //Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
     selectAll: function(cb) {
-        orm.all("burgers", function(res){ 
+        orm.selectAll("burgers", function(res){ 
             cb(res);
         });
     },
     insertOne: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+        orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
     updateOne: function(objColVals, condition, cb) {
-        orm.devour("burgers", objColVals, condition, function(res) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
-        })
+        });
     }
 };
 
